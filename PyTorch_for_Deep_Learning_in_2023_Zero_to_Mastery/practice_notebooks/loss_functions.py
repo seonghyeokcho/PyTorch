65f6ee91589def3fab0b4a9ed480ddaafcfe63c8
seonghyeok cho <whtjdgur77@gmail.com>
2023-08-10 16:38:03 +0900

    Update loss_functions.py

--- PyTorch_for_Deep_Learning_in_2023_Zero_to_Mastery/practice_notebooks/loss_functions.py
+++ PyTorch_for_Deep_Learning_in_2023_Zero_to_Mastery/practice_notebooks/loss_functions.py
@@ -10,13 +10,13 @@ def SSE(y, y_hat):
     return 0.5 * np.sum((y - y_hat)**2)
 
 
-# MSE(Mean Squared Error)
+# MSE(Mean Squared Error) - L2 Loss
 def MSE(y, y_hat):
     N = len(y)
     return (1/N)*np.sum((y - y_hat)**2)
 
 
-# MAE(Mean Absolute Error)
+# MAE(Mean Absolute Error) - L1 Loss
 def MAE(y, y_hat):
     N = len(y)
     return (1/N)*np.sum(abs(y - y_hat))
