165d0302cfeba4bbb9c5e22b1d5c5055979a11a2
seonghyeok cho <whtjdgur77@gmail.com>
2023-08-14 16:08:55 +0900

    Update activation_functions.py

--- PyTorch_for_Deep_Learning_in_2023_Zero_to_Mastery/practice_notebooks/activation_functions.py
+++ PyTorch_for_Deep_Learning_in_2023_Zero_to_Mastery/practice_notebooks/activation_functions.py
@@ -2,4 +2,27 @@ import numpy as np
 
 
 def sigmoid(x):
-    return 1 / 1 + np.exp(x)
+    return 1 / (1 + np.exp(-x))
+
+
+def ReLU(x):
+    return max(0, x)
+
+
+def LeakyReLU(x, alpha):
+    return np.maximum(x*alpha, x)
+
+
+# hyperbolic tangent, tanh(쌍곡 탄젠트)
+def tanh(x):
+    return (np.exp(x)-np.exp(-x)) / (np.exp(x)+np.exp(-x))
+
+
+# hyperbolic cosine, cosh(쌍곡 코사인)
+def cosh(x):
+    return (np.exp(x)-np.exp(-x)) / 2
+
+
+# hyperbolic sine, sinh(쌍곡 사인)
+def sinh(x):
+    return (np.exp(x)+np.exp(-x)) / 2
